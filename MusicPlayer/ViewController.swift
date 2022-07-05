dbfecb214afb17e9bdc9c662a1a1b8c8555b6c9f
ofigensky <bulletproofgleb@icloud.com>
2022-07-05 22:49:10 +0300

    Initial Commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Gleb Glushok on 05.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
