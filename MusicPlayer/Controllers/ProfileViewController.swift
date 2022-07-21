eae6f5e091bd139180ba0a5ba5ef1a571a88e42e
Eugene Kotovich <sxekot@gmail.com>
2022-07-21 14:14:27 +0300

    add profile response

--- MusicPlayer/Controllers/ProfileViewController.swift
+++ MusicPlayer/Controllers/ProfileViewController.swift
@@ -11,7 +11,7 @@ class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = .black
         title = "Profile"
         APICaller.shared.getCurrentUserProfile { result in
             switch result {
